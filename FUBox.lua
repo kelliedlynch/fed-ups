6c4f2b83d287b681cb033933ed04f8c33a7b3187
Kellie Lynch <kelliedlynch@gmail.com>
2013-01-14 18:29:32 -0600

    converted back to using mousejoints

--- FUBox.lua
+++ FUBox.lua
@@ -12,51 +12,10 @@ function _B.new(x,y,size)
 	local body = world:addBody(MOAIBox2DBody.DYNAMIC)
 
 	poly = {
-		box.x,box.y,
-		box.x+self.width,box.y,
-		box.x+self.width,box.y+self.height,
-		box.x,box.y+self.height,
-	}
-
-	local texture = MOAIGfxQuad2D.new()
-	texture:setTexture('Resources/Textures/brown-4x4.png')
-	texture:setRect(box.x, box.y, box.x + size, box.y + size)
-	local sprite = MOAIProp2D.new()
-	--sprite:setLoc(0,0)
-	sprite:setDeck(texture)
-	--sprite:setParent(body)
-	layer:insertProp(sprite)
-
-	box.sprite = sprite
-	box.width = size/2
-	box.height = size/2
-
-	box.damage = 0
-	damageCounter:setString(""..box.damage)
-	boxForProp[sprite] = box
-
-	return box
-end
-
-function _B:releaseBox ()
-	local body = world:addBody(MOAIBox2DBody.DYNAMIC)
-	--body:setAwake(false)
-	print("self.sprite", self.sprite)
-	print("self.sprite:getDims", self.sprite:getDims())
-	print("self.sprite:getLoc", self.sprite:getLoc())
-	print("worldToModel", self.sprite:worldToModel(self.sprite:getLoc()))
-	print("modelToWorld", self.sprite:modelToWorld(self.x, self.y))
-	print("wndToWorld", layer:wndToWorld(self.sprite:getLoc()))
-	print("worldToWnd", layer:worldToWnd(self.sprite:getLoc()))
-	--x, y = self.sprite:getLoc()
-	local x, y = worldX, worldY
-	--local x,y = self.x, self.y
-
-	poly = {
-		x,y,
-		x+self.width,y,
-		x+self.width,y+self.height,
-		x,y+self.height,
+		box.x, box.y,
+		box.x+size, box.y,
+		box.x+size, box.y+size,
+		box.x, box.y+size,
 	}
 
 	local fixture = body:addPolygon (poly)
@@ -70,11 +29,29 @@ function _B:releaseBox ()
 	body:resetMassData()
 
 	boxForFixture[fixture] = self
-	self.sprite.body = body
-	self.body = body
-	self.fixture = fixture
-	self.sprite:setParent(body)
+	box.fixture = fixture
 
+	local texture = MOAIGfxQuad2D.new()
+	texture:setTexture('Resources/Textures/brown-4x4.png')
+	texture:setRect(box.x, box.y, box.x + size, box.y + size)
+	local sprite = MOAIProp2D.new()
+	sprite:setDeck(texture)
+	layer:insertProp(sprite)
+
+	box.sprite = sprite
+	box.width = size/2
+	box.height = size/2
+	box.body = body
+
+	box.sprite:setParent(body)
+	box.sprite.body = body
+
+	box.damage = 0
+	damageCounter:setString(""..box.damage)
+	boxForProp[sprite] = box
+	boxForFixture[fixture] = box
+
+	return box
 end
 
 function _B:updateDamage(box, dmg)
@@ -110,11 +87,12 @@ function onBoxCollision(event, boxFixture, obstacle)
 end
 
 function beginBoxCollision(boxFixture, obstacle)
+	-- print("boxFixture, obstacle", boxFixture, obstacle)
 	velX, velY = boxFixture:getBody():getLinearVelocity()
 	totalVelocity = math.floor(math.abs(velX)) + math.floor(math.abs(velY))
-	box = boxForFixture[boxFixture]
-	local totalDamage = box.damage + math.floor((totalVelocity / 10) * (boxFixture:getBody():getMass() / 200))
-	box:updateDamage(box, totalDamage)
+	local boxObject = boxForFixture[boxFixture]
+	local totalDamage = boxObject.damage + math.floor((totalVelocity / 10) * (boxFixture:getBody():getMass() / 200))
+	boxObject:updateDamage(boxObject, totalDamage)
 end
 
 return _B
--- FUDispatch.lua
+++ FUDispatch.lua
@@ -1,25 +1,28 @@
 -- global table to link box objects with physics objects
 boxForFixture = {}
---
+-- global table to link box objects with their sprites
 boxForProp = {}
 -- activeBox is the box that is currently being scored (the most recent one acted on by the player)
 activeBox = nil
+-- heldBox is the box currently being dragged around by the pointer
+heldBox = nil
 
 function pointerCallback (x, y)
 	worldX, worldY = layer:wndToWorld(x, y)
-	if heldBox then
-		if heldBox.body then
-			heldBox.body:setTransform(worldX, worldY)
-		else
-			print("x,y", x, y)
-			print("worldX, worldY", worldX, worldY)
-			x, y = heldBox.sprite:worldToModel(worldX-heldBox.x-heldBox.width, worldY-heldBox.y-heldBox.height)
-			heldBox.sprite:addLoc(x, y)
-		end
-	end
-	-- if mouseBody then
-	-- 	mouseJoint:setTarget(worldX, worldY)
+	-- if heldBox then
+	-- 	if heldBox.body then
+	-- 		heldBox.body:setTransform(worldX, worldY)
+	-- 	else
+	-- 		print("x,y", x, y)
+	-- 		print("worldX, worldY", worldX, worldY)
+	-- 		x, y = heldBox.sprite:worldToModel(worldX-heldBox.x-heldBox.width, worldY-heldBox.y-heldBox.height)
+	-- 		heldBox.sprite:addLoc(x, y)
+	-- 	end
 	-- end
+	if mouseBody then
+		print("mouseBody")
+		mouseJoint:setTarget(worldX, worldY)
+	end
 
 end
 
@@ -33,97 +36,82 @@ function onDraw ( index, xOff, yOff, xFlip, yFlip )
 end
 
 function clickCallback(down)
+	--local launch
 	-- see if a sprite has been touched/clicked
-	boxClicked = layer:getPartition():propForPoint(worldX, worldY)
+	local boxClicked = layer:getPartition():propForPoint(worldX, worldY)
 	if boxClicked then 
 
 	end
-	print("box", box)
+	print("box", boxClicked)
 	if down then
 		--pointerCallback(worldX, worldY)
-		if box then
+		if boxClicked then
 
 
-			-- print("picking up box")
-			-- -- start displaying damage for this box
-			-- damageCounter:setString(""..boxForProp[box].damage)
+			print("picking up box", boxClicked)
+			heldBox = boxClicked
+			-- start displaying damage for this box
+			damageCounter:setString(""..boxForProp[boxClicked].damage)
 			
-			-- activeBox = boxForProp[box]
-			-- heldBox = boxForProp[box]
-			-- heldBox.body:setTransform(worldX, worldY)
-			-- boxForProp[box].body:setActive(false)
-			-- box.body:setFixedRotation(true)
-			-- box.body:setLinearVelocity(0,0)
-			-- box.body:setAngularVelocity(0)
-			-- mouseBody = world:addBody(MOAIBox2DBody.STATIC)
-			-- mouseBody:setTransform(worldX, worldY)
+			activeBox = boxForProp[boxClicked]
+			heldBox = boxForProp[boxClicked]
+			--heldBox.body:setTransform(worldX, worldY)
+			--boxForProp[boxClicked].body:setActive(false)
+			boxClicked.body:setFixedRotation(true)
+			boxClicked.body:setLinearVelocity(0,0)
+			boxClicked.body:setAngularVelocity(0)
+			mouseBody = world:addBody(MOAIBox2DBody.STATIC)
+			mouseBody:setTransform(worldX, worldY)
 
-			-- mouseJoint = world:addMouseJoint(mouseBody, box.body, worldX, worldY,  10000.0 * box.body:getMass());
+			mouseJoint = world:addMouseJoint(mouseBody, boxClicked.body, worldX, worldY,  10000.0 * boxClicked.body:getMass());
 		else
 			-- note starting position for calculation when launching box
 			launch = true
 			startX, startY = worldX, worldY
 
 			
-			box = FUBox.new(worldX, worldY, 20)
+			local newBox = FUBox.new(worldX, worldY, 20)
 
 			-- activeBox is the box object being scored
-			activeBox = box
+			activeBox = newBox
 			-- heldBox is the box object under the pointer/finger
-			heldBox = box
+			heldBox = newBox
 
-			-- box.body:setFixedRotation(true)
+			newBox.body:setFixedRotation(true)
 
 
-			-- mouseBody = world:addBody(MOAIBox2DBody.STATIC)
+			mouseBody = world:addBody(MOAIBox2DBody.STATIC)
+			mouseJoint = world:addMouseJoint(mouseBody, newBox.body, worldX, worldY,  10000.0 * newBox.body:getMass())
 
-			-- mouseJoint = world:addMouseJoint(mouseBody, box.body, worldX, worldY,  10000.0 * box.body:getMass())
-
-			-- scriptDeck = MOAIScriptDeck.new ()
-			-- scriptDeck:setRect ( 0,0, screenWidth, screenHeight )
-			-- scriptDeck:setDrawCallback ( onDraw )
-			-- lineprop = MOAIProp2D.new ()
-			-- lineprop:setDeck ( scriptDeck )
-			-- HUDLayer:insertProp ( lineprop )
+			local scriptDeck = MOAIScriptDeck.new ()
+			scriptDeck:setRect ( 0,0, screenWidth, screenHeight )
+			scriptDeck:setDrawCallback ( onDraw )
+			launchLine = MOAIProp2D.new ()
+			launchLine:setDeck ( scriptDeck )
+			HUDLayer:insertProp ( launchLine )
 		end
 	else  -- mouseup/touchup event
-		--heldBox = nil
-
-
-		-- if layer:getPartition() then print("checking for box"); box = layer:getPartition():propForPoint(worldX, worldY) end
-		-- if lineprop then
-		-- 	HUDLayer:removeProp(lineprop)
-		-- 	lineprop = nil
-		-- 	scriptDeck = nil
-		-- end
-		-- --if mouseBody then
+		if layer:getPartition() then 
+			print("checking for box") 
+			boxObject = layer:getPartition():propForPoint(worldX, worldY) 
+		end
+		if launchLine then
+			HUDLayer:removeProp(launchLine)
+			launchLine = nil
+			scriptDeck = nil
+		end
 		if heldBox then
-			print("heldBox.sprite", heldBox.sprite)
-		-- 	print("heldBox exists, releasing box")
-		-- 	--also destroys joint
-		-- 	-- mouseBody:destroy()
-		-- 	-- mouseBody = nil
-		-- 	-- print("box on release", box)
-		-- 	-- for k,v in pairs(boxForProp) do
-		-- 	-- 	print("boxForProp", k,v)
-		-- 	-- end
-		-- 	-- print("boxObject", boxForProp[box])
-			heldBox:releaseBox()
-			heldBox = nil
-			-- heldBox.body:setActive(true)
-			--heldBox = nil
-		-- 	local boxObject = boxForProp[box]
-		-- 	boxObject.body:setFixedRotation(false)
-		-- 	if launch then
-		-- 		print("launching box")
-		-- 		--distance = math.sqrt((startX-worldX)^2 + (startY-worldY)^2)
-				
-		-- 		boxObject.body:applyLinearImpulse(5000 * (startX-worldX), 5000 * (startY - worldY), worldX, worldY)
-		-- 		launch = false
-		-- 	end
-		-- 	--box = nil
-		-- else
+			-- also destroys joint
+			mouseBody:destroy()
+			mouseBody = nil
 
+			heldBox.body:setActive(true)
+			heldBox.body:setFixedRotation(false)
+			if launch then
+				heldBox.body:applyLinearImpulse(5000 * (startX-worldX), 5000 * (startY - worldY), worldX, worldY)
+				launch = false
+			end
+			heldBox = nil
 		end
 	end
 end
